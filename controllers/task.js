08660f931f8843c1daca2e18ef8dafdc830203bf
Ajinkya <leeajink@gmail.com>
2023-05-20 12:36:56 +0700

    Added Update Task functionality

--- controllers/task.js
+++ controllers/task.js
@@ -1,4 +1,44 @@
+// const Task = require('../models/Task');
+
+// //GET ALL
+// const getAllTask = (req, res) => {
+//     res.send("All Items")
+// }
+
+
+// //POST
+// const createTask = async (req, res) => {
+//     const task = await Task.create(req.body)
+//     res.status(201).json({task})
+// }
+
+// //GET SINGLE TASK
+// const getTask = (req, res) => {
+//     res.json({id:req.params.id})
+// }
+
+// //UPDATE TASK   
+// const updateTask = (req, res) => {
+//     res.send('Update the task');
+// }
+
+// //DELETE TASK
+// const deleteTask = (req, res) => {
+//     res.send('Delete the task')
+// }
+
+
+// module.exports = {
+//     getAllTask,
+//     createTask,
+//     getTask,
+//     updateTask,
+//     deleteTask
+// };
+
+
 const Task = require('../models/Task');
+
 
 //GET ALL
 const getAllTask = async (req, res) => {
@@ -37,20 +77,16 @@ const getTask = async (req, res) => {
     }
 }
 
-//UPDATE TASK   
-const updateTask = (req, res) => {
-    res.send('Update the task');
-}
 
 //DELETE TASK
 const deleteTask = async (req, res) => {
 
     try {
         const { id } = req.params;
-        const deleteTask = await Task.deleteOne({ _id:id });
+        const deleteTask = await Task.findOneAndDelete({ _id:id });
 
         if(!deleteTask){
-            res.status(404).json({ msg: `No task found to delete ${id}`})
+            res.status(404).json({ msg: `No task found to delete with id - ${id}`})
         }
 
         res.status(200).json({ msg: `Deleted ${id} successfully` });
@@ -58,6 +94,29 @@ const deleteTask = async (req, res) => {
         res.status(500).json({ msg: `Something went wrong`})
     }
 }
+
+
+//UPDATE TASK   
+const updateTask = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const updateTask = await Task.findOneAndUpdate({ _id:id }, req.body, {
+            new:true,
+            runValidators:true,
+        })
+
+        if(!updateTask){
+            res.status(404).json({ msg: `Cannot find that task`})
+        }
+
+        res.status(200).json({ msg: `Update task successfully no - ${id}`})
+    }catch(error){
+        res.status(500).json({ msg: `Cannot update something wrong`})
+    }
+}
+
+
+
 
 
 module.exports = {
